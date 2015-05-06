139041585efe53aa459b73915fc20619b51aa706
Bima <palessie27@gmail.com>
2015-05-05 20:21:41 -0400

    goals.rb

---
+++ My_First_Bewd_Class/goals.rb
@@ -0,0 +1,5 @@
+=begin
+Abimanyu (Bima) Saridjo=end
+
+=begin
+I hope to learn to add membership features to my website project, such as a forum, ability to comment and rate posts and others comments. For the final project, I hope to expand on my FEWD final project.=end
